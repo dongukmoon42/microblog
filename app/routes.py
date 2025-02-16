e46d54d2ec4abf39221db56f309141000eaa0e75
ResoM <dongukmoon42@gmail.com>
2025-02-16 15:52:00 +0900

    Feat : 이름 / 모의 게시물 구현현

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template  # render_template 함수 임포트
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    # 모의 사용자 데이터
+    user = {'username': 'Miguel'}
+
+    # 모의 게시물 데이터
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    # 템플릿 렌더링
+    return render_template('index.html', title='Home', user=user, posts=posts)
